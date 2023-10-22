8746a704c87be7691c82c84a2abf9548c4bfe8be
rolv <rolv.apneseth@gmail.com>
2023-10-22 20:23:26 +0100

    feat: add a very basic usage example

---
+++ examples/basic/src/main.rs
@@ -0,0 +1,17 @@
+use lib_game_detector::{data::SupportedLaunchers, get_detector};
+use tracing::debug;
+
+fn main() {
+    // Init tracing
+    tracing_subscriber::fmt::init();
+
+    debug!("Initialising detector");
+    let detector = get_detector();
+
+    // dbg!(detector.get_detected_launchers());
+    dbg!(detector.get_all_detected_games());
+    // dbg!(detector.get_all_detected_games_per_launcher());
+    // dbg!(detector.get_all_detected_games_with_box_art());
+    dbg!(detector
+        .get_all_detected_games_from_specific_launcher(SupportedLaunchers::HeroicGamesAmazon));
+}

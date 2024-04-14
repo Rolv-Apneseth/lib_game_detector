72cc83c738e1fba6dd62e0b27adc85b6ca689567
rolv <rolv.apneseth@gmail.com>
2024-04-14 19:42:19 +0100

    fix(lutris): detect game box art that uses `slug` instead of `game_slug`, and images of different file formats

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 use std::{
-    path::PathBuf,
+    path::{Path, PathBuf},
     process::Command,
     sync::{Arc, Mutex},
 };
@@ -44,6 +44,17 @@ pub fn some_if_file(path: PathBuf) -> Option<PathBuf> {
 /// Returns an Option containing the given `PathBuf`, if the `PathBuf` points to an actual directory
 pub fn some_if_dir(path: PathBuf) -> Option<PathBuf> {
     path.is_dir().then_some(path)
+}
+
+/// Returns the first existing image file path (based on a set number of image extensions) for a given
+/// directory path and file name
+///
+/// e.g. dir/path/file_name.{png,jpg,jpeg} will return the first path which actually exists (or
+/// `None` if none of them exist)
+pub fn get_existing_image_path(base_path: &Path, file_name: &str) -> Option<PathBuf> {
+    ["png", "jpg", "jpeg"]
+        .iter()
+        .find_map(|ext| some_if_file(base_path.join(format!("{file_name}.{ext}"))))
 }
 
 #[cfg(test)]

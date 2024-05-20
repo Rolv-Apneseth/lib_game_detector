1e1ab9958116934bf4130e2d4200bc16d4f1f6be
rolv <rolv.apneseth@gmail.com>
2024-05-20 22:39:03 +0100

    refactor: remove seemingly unneeded `black_box` calls

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,21 +1,21 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use lib_game_detector::{data::SupportedLaunchers, get_detector};
 
 fn main_benchmarks(c: &mut Criterion) {
     let mut group = c.benchmark_group("main");
 
-    group.bench_function("get_detector", |b| b.iter(|| black_box(get_detector())));
+    group.bench_function("get_detector", |b| b.iter(get_detector));
 
     group.bench_function("get_detected_launchers", |b| {
-        b.iter(|| black_box(get_detector().get_detected_launchers()))
+        b.iter(|| get_detector().get_detected_launchers())
     });
 
     group.bench_function("get_all_detected_games", |b| {
-        b.iter(|| black_box(get_detector().get_all_detected_games()))
+        b.iter(|| get_detector().get_all_detected_games())
     });
 
     group.bench_function("get_all_detected_games_with_box_art", |b| {
-        b.iter(|| black_box(get_detector().get_all_detected_games_with_box_art()))
+        b.iter(|| get_detector().get_all_detected_games_with_box_art())
     });
 
     group.finish();
@@ -26,49 +26,40 @@ fn per_launcher_benchmark(c: &mut Criterion) {
     let detector = get_detector();
 
     group.bench_function("steam", |b| {
-        b.iter(|| {
-            detector
-                .get_all_detected_games_from_specific_launcher(black_box(SupportedLaunchers::Steam))
-        })
+        b.iter(|| detector.get_all_detected_games_from_specific_launcher(SupportedLaunchers::Steam))
     });
 
     group.bench_function("heroic - epic", |b| {
         b.iter(|| {
-            detector.get_all_detected_games_from_specific_launcher(black_box(
+            detector.get_all_detected_games_from_specific_launcher(
                 SupportedLaunchers::HeroicGamesEpicGames,
-            ))
+            )
         })
     });
 
     group.bench_function("heroic - gog", |b| {
         b.iter(|| {
-            detector.get_all_detected_games_from_specific_launcher(black_box(
-                SupportedLaunchers::HeroicGOG,
-            ))
+            detector.get_all_detected_games_from_specific_launcher(SupportedLaunchers::HeroicGOG)
         })
     });
 
     group.bench_function("heroic - amazon", |b| {
         b.iter(|| {
-            detector.get_all_detected_games_from_specific_launcher(black_box(
+            detector.get_all_detected_games_from_specific_launcher(
                 SupportedLaunchers::HeroicGamesAmazon,
-            ))
+            )
         })
     });
 
     group.bench_function("lutris", |b| {
         b.iter(|| {
-            detector.get_all_detected_games_from_specific_launcher(black_box(
-                SupportedLaunchers::Lutris,
-            ))
+            detector.get_all_detected_games_from_specific_launcher(SupportedLaunchers::Lutris)
         })
     });
 
     group.bench_function("bottles", |b| {
         b.iter(|| {
-            detector.get_all_detected_games_from_specific_launcher(black_box(
-                SupportedLaunchers::Bottles,
-            ))
+            detector.get_all_detected_games_from_specific_launcher(SupportedLaunchers::Bottles)
         })
     });
 

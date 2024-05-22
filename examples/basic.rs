eb9ee890a65e7eeefe7b5487192a29fde626d21e
rolv <rolv.apneseth@gmail.com>
2024-05-22 13:08:49 +0100

    refactor: convert basic example to a single file (and remove `dhat` profiling)

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,24 +1,19 @@
+extern crate tracing_subscriber;
+
 use lib_game_detector::{data::SupportedLaunchers, get_detector};
 use tracing::debug;
 
-#[cfg(feature = "dhat-heap")]
-#[global_allocator]
-static ALLOC: dhat::Alloc = dhat::Alloc;
-
 fn main() {
-    #[cfg(feature = "dhat-heap")]
-    let _profiler = dhat::Profiler::new_heap();
-
     // Init tracing
     tracing_subscriber::fmt::init();
 
     debug!("Initialising detector");
     let detector = get_detector();
 
-    // dbg!(detector.get_detected_launchers());
+    dbg!(detector.get_detected_launchers());
     dbg!(detector.get_all_detected_games());
-    // dbg!(detector.get_all_detected_games_per_launcher());
-    // dbg!(detector.get_all_detected_games_with_box_art());
+    dbg!(detector.get_all_detected_games_per_launcher());
+    dbg!(detector.get_all_detected_games_with_box_art());
     dbg!(detector
         .get_all_detected_games_from_specific_launcher(SupportedLaunchers::HeroicGamesAmazon));
 }

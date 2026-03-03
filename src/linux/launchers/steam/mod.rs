ad5916d09aee9ffd13b0193b762832b023c25a51
Rolv Apneseth <rolv.apneseth@gmail.com>
2026-03-03 15:44:11 +0000

    fix(steam): fallback to reading .steam symlinks since ~/.local/share is unreliable (#46)

--- src/linux/launchers/steam/mod.rs
+++ src/linux/launchers/steam/mod.rs
@@ -9,8 +9,12 @@ use std::{
 
 pub use steam_base::Steam;
 pub use steam_shortcuts::SteamShortcuts;
+use tracing::error;
 
-use crate::utils::{get_launch_command, get_launch_command_flatpak};
+use crate::{
+    data::SupportedLaunchers,
+    utils::{get_launch_command, get_launch_command_flatpak},
+};
 
 fn get_steam_launch_command(app_id: impl Display, is_using_flatpak: bool) -> Command {
     let game_run_arg = format!("steam://rungameid/{app_id}");
@@ -22,8 +26,33 @@ fn get_steam_launch_command(app_id: impl Display, is_using_flatpak: bool) -> Com
     }
 }
 
-fn get_steam_dir(path_data: &Path) -> PathBuf {
-    path_data.join("Steam")
+fn get_steam_dir(path_home: &Path, path_data: &Path) -> PathBuf {
+    let path = path_data.join("Steam");
+    if path.is_dir() {
+        return path;
+    }
+
+    // Fallback to reading ~/.steam/root or ~/.steam/steam symlinks
+    // See: https://github.com/Rolv-Apneseth/lib_game_detector/issues/45
+    let is_valid = |p: &Path| p.is_symlink() && p.is_dir();
+    let mut symlink = path_home.join(".steam/root");
+    if !is_valid(&symlink) {
+        symlink = path_home.join(".steam/steam");
+    }
+
+    if !is_valid(&symlink) {
+        return path;
+    }
+
+    symlink
+        .canonicalize()
+        .inspect_err(|e| {
+            error!(
+                "{} - Could not canonicalize symlink: {e}",
+                SupportedLaunchers::Steam
+            )
+        })
+        .unwrap_or(path)
 }
 
 fn get_steam_flatpak_dir(path_home: &Path) -> PathBuf {
